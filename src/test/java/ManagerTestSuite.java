55549d2a54dfc8f04a997c9b02f75b26509bb01e
DESKTOP-97S67UE\user <bluetoon@naver.com>
2021-11-17 19:30:09 +0900

    test suite append

--- src/test/java/ManagerTestSuite.java
+++ src/test/java/ManagerTestSuite.java
@@ -1,18 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import user.Manager;
-import user.Player;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.*;
+
+import user.Manager;
 
 
 public class ManagerTestSuite {
-    /*
-     *   안 되는 것을 위주로 테스트 해보자
-     * */
 
     @Test
+    @DisplayName("랜덤 넘버 생성 테스트 ")
     public void when_manager_generate_3_numbers() {
 
         // given
@@ -25,6 +22,7 @@ public class ManagerTestSuite {
     }
 
     @Test
+    @DisplayName("1볼 테스트")
     public void when_manager_verify_1B() {
         // given
         StringBuffer testPlayerNumber = new StringBuffer(3);
@@ -44,6 +42,7 @@ public class ManagerTestSuite {
     }
 
     @Test
+    @DisplayName("2볼 테스트")
     public void when_manager_verify_2B() {
         // given
         StringBuffer testPlayerNumber = new StringBuffer(3);
@@ -63,6 +62,7 @@ public class ManagerTestSuite {
     }
 
     @Test
+    @DisplayName("3볼 테스트")
     public void when_manager_verify_3B() {
         // given
         StringBuffer testPlayerNumber = new StringBuffer(3);
@@ -79,6 +79,117 @@ public class ManagerTestSuite {
 
         // then
         assert manager.em.ball_count == 3;
+    }
+
+    @Test
+    @DisplayName("1볼 1스트라이크 테스트")
+    public void when_manager_verify_1B_1S() {
+        // given
+        StringBuffer testPlayerNumber = new StringBuffer(3);
+        StringBuffer testManagerNumber = new StringBuffer(3);
+        Manager manager = new Manager();
+
+        // when
+        testManagerNumber.append("123");
+        manager.managerNumber = testManagerNumber;
+
+        testPlayerNumber.append("329");
+        manager.setPlayerNumber(testPlayerNumber);
+        manager.verify();
+
+        // then
+
+        assertEquals(1, manager.em.ball_count);
+        assertEquals(1, manager.em.strike_count);
+
+    }
+
+    @Test
+    @DisplayName("2볼 1스트라이크 테스트")
+    public void when_manager_verify_2B_1S() {
+        // given
+        StringBuffer testPlayerNumber = new StringBuffer(3);
+        StringBuffer testManagerNumber = new StringBuffer(3);
+        Manager manager = new Manager();
+
+        // when
+        testManagerNumber.append("123");
+        manager.managerNumber = testManagerNumber;
+
+        testPlayerNumber.append("132");
+        manager.setPlayerNumber(testPlayerNumber);
+        manager.verify();
+
+        // then
+        assertEquals(2, manager.em.ball_count);
+        assertEquals(1, manager.em.strike_count);
+
+    }
+
+    @Test
+    @DisplayName("1스트라이크 테스트")
+    public void when_manager_verify_1S() {
+        // given
+        StringBuffer testPlayerNumber = new StringBuffer(3);
+        StringBuffer testManagerNumber = new StringBuffer(3);
+        Manager manager = new Manager();
+
+        // when
+        testManagerNumber.append("123");
+        manager.managerNumber = testManagerNumber;
+
+        testPlayerNumber.append("189");
+        manager.setPlayerNumber(testPlayerNumber);
+        manager.verify();
+
+        // then
+        assertEquals(0, manager.em.ball_count);
+        assertEquals(1, manager.em.strike_count);
+
+    }
+
+    @Test
+    @DisplayName("2스트라이크 테스트")
+    public void when_manager_verify_2S() {
+        // given
+        StringBuffer testPlayerNumber = new StringBuffer(3);
+        StringBuffer testManagerNumber = new StringBuffer(3);
+        Manager manager = new Manager();
+
+        // when
+        testManagerNumber.append("123");
+        manager.managerNumber = testManagerNumber;
+
+        testPlayerNumber.append("124");
+        manager.setPlayerNumber(testPlayerNumber);
+        manager.verify();
+
+        // then
+        assertEquals(0, manager.em.ball_count);
+        assertEquals(2, manager.em.strike_count);
+
+    }
+
+    @Test
+    @DisplayName("3스트라이크 테스트")
+    public void when_manager_verify_3S() {
+        // given
+        StringBuffer testPlayerNumber = new StringBuffer(3);
+        StringBuffer testManagerNumber = new StringBuffer(3);
+        Manager manager = new Manager();
+
+        // when
+        testManagerNumber.append("123");
+        manager.managerNumber = testManagerNumber;
+
+        testPlayerNumber.append("123");
+        manager.setPlayerNumber(testPlayerNumber);
+        manager.verify();
+
+        // then
+        assertEquals(0, manager.em.ball_count);
+        assertEquals(3, manager.em.strike_count);
+
     }
 
 }
